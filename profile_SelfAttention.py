6c4edb2d89719c4f1f3b3e2e448b7f5ad61755a4
Kamran Chitsaz <kmchiti@Kamrans-MacBook-Pro.local>
2023-08-11 18:58:13 -0400

    add profiler with cuda nvtx

---
+++ profile_SelfAttention.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn.functional as F
+from model import Block, GPTConfig
+
+config = GPTConfig()
+config.block_size = 1024
+config.vocab_size = 2048
+config.n_head = 12
+config.n_embd = 768
+batch_size = 256
+warmup_steps = 10  # warmup_steps for profiler
+active_steps = 4  # active_steps for profiler
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    # raise "CUDA not available"
+    device = torch.device('cpu')
+
+decoder = Block(config).to(device)
+lm_head = torch.nn.Linear(config.n_embd, config.vocab_size, bias=False).to(device)
+decoder.train()
+lm_head.train()
+
+hidden_states = torch.randn(batch_size, 1, config.n_embd).to(device)
+targets = torch.randint(0, config.vocab_size, size=(batch_size, 1)).to(device)
+
+if __name__ == '__main__':
+    for step in range(1000):
+        if step >= warmup_steps + active_steps:
+            torch.cuda.cudart().cudaProfilerStop()
+            quit()
+        elif step >= warmup_steps:
+            torch.cuda.cudart().cudaProfilerStart()
+            out = decoder(hidden_states)[0]
+            logits = lm_head(out)
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+            torch.cuda.nvtx.range_push("backward")
+            loss.backward()
+            torch.cuda.nvtx.range_pop()
+        else:
+            out = decoder(hidden_states)[0]
+            logits = lm_head(out)
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+            loss.backward()
